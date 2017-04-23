4c9dfaf1107afa1101b9324df9c78185ad2bf8a2
masyasha <richello3027@gmail.com>
2017-04-23 17:24:37 +0300

    1.4 BD

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 8080;
 const io = require('socket.io')(http);
 const mongo = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/chat';
+const express = require('express');
+const bodyParser = require('body-parser');
 
 function __getAllMessages(db, callback) {
     db.collection('messages').find().toArray(callback);
@@ -42,14 +44,56 @@ function saveMessages(socket, message) {
     });
 }
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+function __saveUserData(db, name, email, password, callback) {
+    db.collection('userData').insertOne({"_id": name, "email": email, "password": password}, function(err, result) {
+    if (err.name == 'MongoError' && err.code == 11000) {
+        throw err; // write logics for the same name case (nick must be original
+    }
+    callback();
+    });
+}
+
+function saveUserData(name, email, password) {
+    mongo.connect(url, function (err, db) {
+        __saveUserData(db, name, email, password, function () {
+            db.close();
+        });
+    })
+}
+
+function testUserDataSaving(callback) {
+    mongo.connect(url, function (err, db) {
+        console.log(db.collection('userData').find().toArray(callback));
+        db.close();
+        if (err) {
+            next(err)
+        }
+    });
+
+}
+
+app.post('/', function (req, res) {
+    userName = req.body.user.name;
+    userEmail = req.body.user.email;
+    userPassword = req.body.user.password;
+    saveUserData(userName, userEmail, userPassword);
+    testUserDataSaving(function(err, result) {
+        console.log("result: " + result);
+        if (err) {
+            next(err);
+        }});
+    res.send("It's okay");
+});
 
 app.get('/', function(req, res) {
-    res.render('index.ejs', { title: "Testing! Now with MongoDB!"});
+    res.render('page.ejs', { title: "Testing! Now with MongoDB!"});
 });
 
 io.on('connection', function (socket) {
-
-    console.log("user connected");
 
     getMessages(socket);
 
@@ -67,11 +111,9 @@ http.listen(port, function () {
     mongo.connect(url, function(err, db) {
         try {
             db.collection('messages').remove({});
-            console.log("BD cleaned")
         }
         catch (err) {}
         db.close();
     });
     console.log("Listening on port " + port + "....");
 });
-
