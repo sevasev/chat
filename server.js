07df5db3812bdc82a3927e965755ecb60ea0e8f5
masyasha <richello3027@gmail.com>
2017-04-25 21:08:21 +0300

    1.4 niice

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const port = 8080;
+const express = require('express');
 const io = require('socket.io')(http);
 const mongo = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/chat';
-const express = require('express');
 const bodyParser = require('body-parser');
+const basicAuth = require('express-basic-auth');
+
+const port = 8080;
+const url = 'mongodb://localhost:27017/chat';
 
 function __getAllMessages(db, callback) {
     db.collection('messages').find().toArray(callback);
@@ -48,49 +50,55 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-function __saveUserData(db, name, email, password, callback) {
-    db.collection('userData').insertOne({"_id": name, "email": email, "password": password}, function(err, result) {
-    if (err.name == 'MongoError' && err.code == 11000) {
+function __saveUserData(db, username, password, callback) {
+    db.collection('userData').insertOne({"username": username, "password": password}, function(err) {
+    if (err) {
         throw err; // write logics for the same name case (nick must be original
     }
     callback();
     });
 }
 
-function saveUserData(name, email, password) {
+function saveUserData(username, password, res) {
     mongo.connect(url, function (err, db) {
-        __saveUserData(db, name, email, password, function () {
-            db.close();
+        let save = true;
+        db.collection('userData').find().toArray(function (err, result) {
+            let len = result.length;
+            for (let i = 0; i < len; i++) {
+                if (result[i].username == username) {
+                    save = false;
+                }
+            }
+            if (save === true) {
+                __saveUserData(db, username, password, function () {
+                    db.close();
+                });
+                formPassed(res);
+            } else {
+                formFailed(res)
+            }
         });
-    })
-}
-
-function testUserDataSaving(callback) {
-    mongo.connect(url, function (err, db) {
-        console.log(db.collection('userData').find().toArray(callback));
-        db.close();
-        if (err) {
-            next(err)
-        }
     });
 
 }
 
+
 app.post('/', function (req, res) {
-    userName = req.body.user.name;
-    userEmail = req.body.user.email;
+    userName = req.body.user.username;
     userPassword = req.body.user.password;
-    saveUserData(userName, userEmail, userPassword);
-    testUserDataSaving(function(err, result) {
-        console.log("result: " + result);
-        if (err) {
-            next(err);
-        }});
-    res.send("It's okay");
+    saveUserData(userName, userPassword, res);
 });
 
+function formPassed(res) {
+    res.render('index.ejs', { title: "authorized!!"});
+}
+
+function formFailed(res) {
+    res.render('errNormalPage.ejs', { title: "error!" } );
+}
+
 app.get('/', function(req, res) {
-    res.render('page.ejs', { title: "Testing! Now with MongoDB!"});
+    res.render('page.ejs', { title: "authorizating!"});
 });
 
 io.on('connection', function (socket) {
@@ -117,3 +125,31 @@ http.listen(port, function () {
     });
     console.log("Listening on port " + port + "....");
 });
+
+
+
+
+
+// ADDITIONAL SOURCES
+
+
+// extracting data from DB
+// function testUserDataSaving(callback) {
+//     mongo.connect(url, function (err, db) {
+//         db.collection('userData').find().toArray(callback);
+//         db.close();
+//         if (err) {
+//             next(err)
+//         }
+//     });
+// }
+
+
+// beginning of authentification
+// basic authorization
+// app.use(basicAuth({ authorizer: auth }));
+//
+// function auth(username, password) {
+//     const result = 0;
+//     return result;
+// }
